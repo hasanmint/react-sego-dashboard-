05443074ceb04bfe4c47347b8b6c5419f241138b
Md. Hasan Ali <hasanmint@example.com>
2022-08-04 06:16:37 +0600

    Initial setup react route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes,Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome To Sego React Dashboard  </h1>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        {/* <Route path="about" element={<About />} /> */}
+      </Routes>
     </div>
   );
 }
