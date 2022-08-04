1e4383d0b56fa8617e98261695d602381af23f05
Md. Hasan Ali <hasanmint@example.com>
2022-08-04 09:59:11 +0600

    added Home page Cards

--- src/components/Dashboard/Main/ContainerPage.js
+++ src/components/Dashboard/Main/ContainerPage.js
@@ -3,7 +3,86 @@ import React from 'react';
 const ContainerPage = () => {
     return (
         <>
-            Container Page
+            <div class="page-wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xl-3 col-sm-6 col-12">
+                            <div class="card card-bg">
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <div class="media d-flex">
+                                            <div class="media-body text-left">
+                                                <h3 class="danger">459</h3>
+                                                <span>Total Menus</span>
+                                            </div>
+                                            <div class="align-self-center">
+                                            <i class="fas fa-bars fa-3x"></i>
+                                            
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-sm-6 col-12">
+                            <div class="card card-bg">
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <div class="media d-flex">
+                                            <div class="media-body text-left">
+                                                <h3 class="success">$ 87,561</h3>
+                                                <span>Total Revenue</span>
+                                            </div>
+                                            <div class="align-self-center">
+                                            <i class="fas fa-dollar-sign fa-3x"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-3 col-sm-6 col-12">
+                            <div class="card card-bg">
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <div class="media d-flex">
+                                            <div class="media-body text-left">
+                                                <h3 class="warning">247</h3>
+                                                <span>Total Oders</span>
+                                            </div>
+                                            <div class="align-self-center">
+                                            <i class="fas fa-cart-plus fa-3x"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-sm-6 col-12">
+                            <div class="card card-bg">
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <div class="media d-flex">
+                                            <div class="media-body text-left">
+                                                <h3 class="primary">872</h3>
+                                                <span>Total Customers</span>
+                                            </div>
+                                            <div class="align-self-center">
+                                            <i class="fas fa-users fa-3x"></i>
+                                            
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
         </>
     );
 };
